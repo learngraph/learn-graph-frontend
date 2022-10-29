6aa3f2618efcf2046cca8b219e182449d0e7ee09
skep <skep@freq>
2022-10-29 14:49:06 +0200

    feat: add langugage & authentication http-header
    Note: they have static content for now. Still need to implement user
    management and language switching in the UI.

--- src/rpc/link.test.ts
+++ src/rpc/link.test.ts
@@ -1,6 +1,22 @@
-//import { gql } from "@apollo/client";
-import { client } from "./link";
-test("client", () => {
-  expect(client).not.toBe(undefined);
-  // TODO: actual tests..
+import { addAuthHeader, addLanguageHeader } from "./link";
+
+describe("link", () => {
+  describe("addAuthHeader", () => {
+    it("should add authorization header", () => {
+      // @ts-ignore
+      let obj = addAuthHeader(undefined, { headers: { a: "B" } });
+      expect(obj.headers).toEqual({
+        a: "B",
+        Authentication: "unauthenticated",
+      });
+    });
+  });
+  describe("addLanguageHeader", () => {
+    it("should add language header", () => {
+      // @ts-ignore
+      let obj = addLanguageHeader(undefined, { headers: { a: "B" } });
+      expect(obj.headers).toEqual({ a: "B", Language: "en" });
+      expect(obj.headers).toHaveProperty("Language");
+    });
+  });
 });
--- src/rpc/link.ts
+++ src/rpc/link.ts
@@ -4,16 +4,41 @@ import {
   ApolloLink,
   HttpLink,
 } from "@apollo/client";
+import { setContext, ContextSetter } from "@apollo/client/link/context";
 
 import fetch from "cross-fetch";
 
-const link: ApolloLink = new HttpLink({
+const linkHttp: ApolloLink = new HttpLink({
   uri: process.env.REACT_APP_BACKEND_DN,
   fetch,
 });
 const cache = new InMemoryCache();
 
+export const addAuthHeader: ContextSetter = (_, { headers }) => {
+  // TODO(skep): user authentication?
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authentication: token ? `Bearer ${token}` : "unauthenticated",
+    },
+  };
+};
+const linkAuth = setContext(addAuthHeader);
+
+export const addLanguageHeader: ContextSetter = (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      Language: "en", // TODO(skep): create context w/ language setting und use it's language value here
+    },
+  };
+};
+const linkLang = setContext(addLanguageHeader);
+
 export const client = new ApolloClient({
-  cache,
-  link,
+  cache: cache,
+  link: linkLang.concat(linkAuth.concat(linkHttp)),
 });
