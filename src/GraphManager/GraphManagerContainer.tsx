0dfbced5ad0d8b6c2b6b21ac78f2f71cf5c5f6ca
Jonas Lamperti <jonas.lamperti@web.de>
2022-11-26 14:50:33 +0100

    add graph for cetup.id

--- src/GraphManager/GraphManagerContainer.tsx
+++ src/GraphManager/GraphManagerContainer.tsx
@@ -3,6 +3,7 @@ import crypto1 from "../graphdata/crypto-1";
 import crypto1b from "../graphdata/crypto-1b";
 import crypto2 from "../graphdata/crypto-2";
 import math1 from "../graphdata/math-1";
+import startup from "../graphdata/startup";
 import { useGraphData } from "./hooks";
 import { GraphManager } from "./GraphManager";
 
@@ -11,6 +12,7 @@ const datasets: DataSetType[] = [
   { dataSetName: "crypto1b", data: crypto1b },
   { dataSetName: "crypto2", data: crypto2 },
   { dataSetName: "math1", data: math1 },
+  { dataSetName: "startup", data: startup }
 ];
 
 export const GraphManagerContainer = (): JSX.Element => {
