ec95d94f0d56a6576716b07f67647ed63eb04eb3
skep <skep@freq>
2023-03-16 23:37:01 +0100

    zoom: refactor: move zoom functions to separate file

--- src/GraphManager/GraphRenderer.tsx
+++ src/GraphManager/GraphRenderer.tsx
@@ -6,6 +6,7 @@ import ForceGraph2D, {
   NodeObject,
 } from "react-force-graph-2d";
 import { GraphData, LinkType, NodeType /*, GraphData*/ } from "./types";
+import { ZoomFn, GraphDataMerged, ZoomDirection, zoomStep } from "./Zoom";
 
 // TODO(skep): fundamental type issue here, we have a NodeType !=
 // ForceGraph2D.NodeObject, and a LinkType != ForceGraph2D.LinkObject , but we
@@ -146,161 +147,6 @@ const onLinkHover = (_: LinkObject | null): void => {
 };
 
 // global input listeners
-
-// TODO(skep): extract zoom functions to separate file
-export interface HasID {
-  id: string;
-}
-export interface LinkBetweenHasIDs {
-  source: HasID;
-  target: HasID;
-  value?: number;
-}
-// HasID and LinkBetweenHasIDs is the zoom interface to the force-graph, since
-// all it needs are objects with IDs and links between objects with IDs.
-export interface GraphDataMerged {
-  nodes: HasID[];
-  links: LinkBetweenHasIDs[];
-}
-
-interface ZoomFn {
-  (args: ZoomArgs): void;
-}
-
-export enum ZoomDirection {
-  In,
-  Out,
-}
-
-export interface ZoomArgs {
-  // number of nodes to merge/un-merge when zooming In/Out
-  steps: number;
-  // zoom In or Out
-  direction: ZoomDirection;
-  // the graph data currently in use by force, that will be modified by a zoom
-  // operation
-  graphData: GraphDataMerged;
-}
-
-// zoomStep performs `steps` zoom steps, where zooming in by N steps merges N
-// nodes into other nodes, reducing the total node count by N.
-export const zoomStep: ZoomFn = (args: ZoomArgs): void => {
-  if (args.steps >= args.graphData.nodes.length) {
-    return;
-  }
-  // select node to merge:
-  const nodesByLinkCount = args.graphData.nodes
-    .map((node) => ({
-      weight: calculateNodeWeight(node, args.graphData.links),
-      node: node,
-    }))
-    .sort((a, b) => b.weight - a.weight);
-  let mergeTargetNode = nodesByLinkCount[0].node;
-  // modify graph:
-  // find first order nodes, as long as the first order links still exist
-  let firstOrderNodesByWeight = args.graphData.links
-    .filter((link) => link.target.id === mergeTargetNode.id)
-    .map((link) => link.source)
-    .map((node) => ({
-      weight: calculateNodeWeight(node, args.graphData.links),
-      node: node,
-    }))
-    .sort((a, b) => a.weight - b.weight)
-    .map((o) => o.node);
-  if (firstOrderNodesByWeight.length === 0) {
-    return; // nothing left to merge
-  }
-  let nodesToRemove = firstOrderNodesByWeight.slice(0, args.steps);
-  // find links, that will stay unchanged to override current link list later
-  let linksToKeep = args.graphData.links.filter(
-    (link) =>
-      !nodesToRemove.find(
-        (removedNode) =>
-          link.source.id === removedNode.id &&
-          link.target.id === mergeTargetNode.id
-      )
-  );
-  // rewrite links from deleted nodes to 2nd order nodes, removing duplicates
-  rewrite2ndOrderLinks(nodesToRemove, linksToKeep, mergeTargetNode, {
-    deleted: "source",
-    other: "target",
-  });
-  // rewrite links from 2nd order nodes to mergeTargetNode, removing duplicates
-  rewrite2ndOrderLinks(nodesToRemove, linksToKeep, mergeTargetNode, {
-    deleted: "target",
-    other: "source",
-  });
-  // delete all nodesToRemove
-  deleteFromArray(args.graphData.nodes, nodesToRemove);
-  // delete first order links
-  replaceArray(args.graphData.links, linksToKeep);
-  // recurse if we didn't merge enough nodes yet
-  args.steps -= nodesToRemove.length;
-  if (args.steps > 0) {
-    zoomStep(args);
-  }
-};
-
-function deleteFromArray(nodes: HasID[], nodesToRemove: HasID[]) {
-  let leftOverNodes = nodes.filter(
-    (node) => !nodesToRemove.find((findNode) => node.id === findNode.id)
-  );
-  replaceArray(nodes, leftOverNodes);
-}
-
-// replace the content of `ar` with `newar` (in-place operation)
-function replaceArray<T>(a: T[], newa: T[]) {
-  a.splice(0, a.length, ...newa);
-}
-
-// rewrite2ndOrderLinks rewrites 2nd order links to skip the deleted nodes,
-// `dir` specifies the link's direction
-const rewrite2ndOrderLinks = (
-  nodesToRemove: HasID[],
-  linksToKeep: LinkBetweenHasIDs[],
-  mergeTargetNode: HasID,
-  dir: { deleted: "source" | "target"; other: "source" | "target" }
-) => {
-  let secondOrderSourceLinks = nodesToRemove.flatMap((firstOrderNode) => {
-    return linksToKeep.filter(
-      (link) => link[dir.deleted].id === firstOrderNode.id
-    );
-  });
-  secondOrderSourceLinks.forEach((link) => {
-    const index = linksToKeep.findIndex(
-      (existingLink) =>
-        existingLink[dir.deleted].id === mergeTargetNode.id &&
-        existingLink[dir.other].id === link[dir.other].id
-    );
-    if (index !== -1) {
-      linksToKeep.splice(index, 1);
-    }
-    link[dir.deleted] = mergeTargetNode;
-    if (link[dir.deleted] === link[dir.other]) {
-      linksToKeep.splice(
-        linksToKeep.findIndex((selfLink) => link === selfLink),
-        1
-      );
-    }
-  });
-};
-
-// calculates node weight by first order link count, weighted by link.value
-export const calculateNodeWeight = (
-  node: HasID,
-  links: LinkBetweenHasIDs[]
-) => {
-  const defaultWeight = 1;
-  const weightIncrementPerLink = 1;
-  return links.reduce((weight, link) => {
-    if (link.target.id === node.id) {
-      return weight + weightIncrementPerLink * (link.value ?? defaultWeight);
-    } else {
-      return weight;
-    }
-  }, 0 /*init value for count*/);
-};
-
 export const makeKeydownListener = (
   zoom: ZoomFn,
   graphData: GraphDataMerged
@@ -331,6 +177,7 @@ const config = {
   font: "Sans-Serif",
 };
 
+// TODO: extract to another file
 const transformToRenderedType = (graph: TranslatedGraphData): GraphData => {
   // TODO: use language context
   const language = "en";
@@ -355,9 +202,9 @@ export const GraphRenderer = (props: GraphRendererProps) => {
   );
   const onLinkClick = onLinkClickFn(props);
   // TODO(j): is this the react way of listening for input?
+  // FIXME(skep): this listener is called 4-times per key-press - why?!
   document.addEventListener(
     "keydown",
-    // @ts-ignore (┙>∧<)┙へ┻┻
     makeKeydownListener(zoomStep, graphDataForRender)
   );
   return (
---
+++ src/GraphManager/Zoom.test.tsx
@@ -0,0 +1,480 @@
+import {
+  zoomStep,
+  ZoomDirection,
+  calculateNodeWeight,
+  GraphDataMerged,
+  ZoomArgs,
+  LinkBetweenHasIDs,
+  HasID,
+} from "./Zoom";
+
+describe("zoom", () => {
+  describe("in", () => {
+    let nodeList = [
+      { id: "A" },
+      { id: "B" },
+      { id: "C" },
+      { id: "D" },
+      { id: "E" },
+    ];
+    let node = Object.assign(
+      // @ts-ignore
+      ...nodeList.map((node) => ({ [node.id]: node }))
+    );
+    it.each([
+      [
+        // test name
+        "zoom a single step",
+        // input state visualized:
+        "A -> B <- C",
+        // expected output state visualized:
+        "B <- C",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.B },
+            ],
+          },
+        },
+        {
+          nodes: [node.B, node.C],
+          links: [{ source: node.C, target: node.B }],
+        },
+      ],
+      [
+        "do nothing when steps == 0",
+        "A -> B",
+        "A -> B",
+        {
+          steps: 0,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B],
+            links: [{ source: node.A, target: node.B }],
+          },
+        },
+        {
+          nodes: [node.A, node.B],
+          links: [{ source: node.A, target: node.B }],
+        },
+      ],
+      [
+        "do nothing when no nodes can be deleted",
+        "A; B",
+        "A; B",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B],
+            links: [],
+          },
+        },
+        {
+          nodes: [node.A, node.B],
+          links: [],
+        },
+      ],
+      [
+        "zoom 2 steps",
+        "A -> B <- C",
+        "B",
+        {
+          steps: 2,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.B },
+            ],
+          },
+        },
+        { nodes: [node.B], links: [] },
+      ],
+      [
+        "cannot zoom in further than the amount of nodes available",
+        "A -> B <- C",
+        "A -> B <- C",
+        {
+          steps: 3,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.B },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.B, node.C],
+          links: [
+            { source: node.A, target: node.B },
+            { source: node.C, target: node.B },
+          ],
+        },
+      ],
+      [
+        "rewrite links to merged nodes",
+        "A -> B -> C <- D",
+        "A -> C",
+        {
+          steps: 2,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.B, target: node.C },
+              { source: node.D, target: node.C },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.C],
+          links: [{ source: node.A, target: node.C }],
+        },
+      ],
+      [
+        "preserve forward links",
+        "A -> B <- C; B -> D",
+        "B -> D",
+        {
+          steps: 2,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.B },
+              { source: node.B, target: node.D },
+            ],
+          },
+        },
+        {
+          nodes: [node.B, node.D],
+          links: [{ source: node.B, target: node.D }],
+        },
+      ],
+      [
+        "choose mergeTargetNode by node order on equal weight",
+        "A -> B; C -> D",
+        "B; C -> D",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.D },
+            ],
+          },
+        },
+        {
+          nodes: [node.B, node.C, node.D],
+          links: [{ source: node.C, target: node.D }],
+        },
+      ],
+      [
+        "removed node's links, must be moved",
+        "A <- B -> C <- D",
+        "A <- C <- D",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.B, target: node.A },
+              { source: node.B, target: node.C },
+              { source: node.D, target: node.C },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.C, node.D],
+          links: [
+            { source: node.C, target: node.A },
+            { source: node.D, target: node.C },
+          ],
+        },
+      ],
+      [
+        "remove duplicate links, after link target forwarding (secondOrderTargetLinks)",
+        // FIXME: got to complex due to other changes to the zoomStep function
+        // 1. ensure that C stays mergeTargetNode: B -2-> C
+        // 2. ensure A gets deleted (lower node-weight, than B): D -2-> A
+        "D -2-> A -> B -2-> C; A -> C",
+        "D -2-> A -> C",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.B, target: node.C, value: 2 },
+              { source: node.A, target: node.B },
+              { source: node.A, target: node.C },
+              { source: node.D, target: node.A, value: 2 },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.C, node.D],
+          links: [
+            { source: node.A, target: node.C },
+            { source: node.D, target: node.A, value: 2 },
+          ],
+        },
+      ],
+      [
+        "remove duplicate links, after link source forwarding (secondOrderSourceLinks)",
+        "D <- A -> B <- C; B -> D",
+        "D <- B <- C",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.A, target: node.D },
+              { source: node.B, target: node.D },
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.B },
+            ],
+          },
+        },
+        {
+          nodes: [node.B, node.C, node.D],
+          links: [
+            { source: node.B, target: node.D },
+            { source: node.C, target: node.B },
+          ],
+        },
+      ],
+      [
+        "select deleted nodes by weight (1/2)",
+        "A -2-> B -2-> C <-2- D <- E",
+        "A -> B -> C <- E",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D, node.E],
+            links: [
+              { source: node.A, target: node.B, value: 2 },
+              { source: node.B, target: node.C, value: 2 },
+              { source: node.D, target: node.C, value: 2 },
+              { source: node.E, target: node.D },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.B, node.C, node.E],
+          links: [
+            { source: node.A, target: node.B, value: 2 },
+            { source: node.B, target: node.C, value: 2 },
+            { source: node.E, target: node.C },
+          ],
+        },
+      ],
+      [
+        "select deleted nodes by weight (2/2)",
+        "A -> B -2-> C <-2- D <-2- E",
+        "A -> C <- D <- E",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D, node.E],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.B, target: node.C, value: 2 },
+              { source: node.D, target: node.C, value: 2 },
+              { source: node.E, target: node.D, value: 2 },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.C, node.D, node.E],
+          links: [
+            { source: node.A, target: node.C },
+            { source: node.D, target: node.C, value: 2 },
+            { source: node.E, target: node.D, value: 2 },
+          ],
+        },
+      ],
+      [
+        "doubly outgoing links on first order deleted nodes",
+        "A <- B -> C",
+        "A -> C",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C],
+            links: [
+              { source: node.B, target: node.A },
+              { source: node.B, target: node.C },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.C],
+          links: [{ source: node.A, target: node.C }],
+        },
+      ],
+      [
+        "self-linking node with otherwise only source links",
+        "A -10-> A; C <- A -> B",
+        "TODO: WTF?!",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C],
+            links: [
+              { source: node.A, target: node.A, value: 10 },
+              { source: node.A, target: node.B, value: 2 },
+              { source: node.A, target: node.C, value: 2 },
+            ],
+          },
+        },
+        {
+          nodes: [/*node.A,*/ node.B, node.C],
+          links: [
+            //{ source: node.A, target: node.A, value: 10 },
+            //{ source: node.A, target: node.B, value: 2 },
+            //{ source: node.A, target: node.C, value: 2 },
+          ],
+        },
+      ],
+      [
+        "2 zoom steps in separated graph (enforce recursion)",
+        "A -> B; C -> D",
+        "B; D",
+        {
+          steps: 2,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.A, target: node.B },
+              { source: node.C, target: node.D },
+            ],
+          },
+        },
+        {
+          nodes: [node.B, node.D],
+          links: [],
+        },
+      ],
+      [
+        "remove self-referencing links after link rewriting",
+        "D -1.5-> A -> B <-> C",
+        "D -1.5-> A -> B",
+        {
+          steps: 1,
+          direction: ZoomDirection.In,
+          graphData: {
+            nodes: [node.A, node.B, node.C, node.D],
+            links: [
+              { source: node.D, target: node.A, value: 1.5 },
+              { source: node.A, target: node.B },
+              { source: node.B, target: node.C },
+              { source: node.C, target: node.B },
+            ],
+          },
+        },
+        {
+          nodes: [node.A, node.B, node.D],
+          links: [
+            { source: node.D, target: node.A, value: 1.5 },
+            { source: node.A, target: node.B },
+          ],
+        },
+      ],
+      //[
+      //  "name",
+      //  "A -> B",
+      //  "A -> B",
+      //  {
+      //    steps: ?,
+      //    direction: ZoomDirection.In,
+      //    graphData: {},
+      //  },
+      //  {},
+      //],
+    ])(
+      "%s: should merge %p to %p",
+      (
+        _test_name: string,
+        _from: string,
+        _to: string,
+        input: ZoomArgs,
+        expected: GraphDataMerged
+      ) => {
+        zoomStep(input);
+        expect(input.graphData).toEqual(expected);
+      }
+    );
+    it.todo("link.weight propagation (force simmulation weight)"); // XXX: probably already correctly done
+    it.todo(
+      "node.weight propagation on merge (node selection weight for merge)"
+    );
+  });
+  describe("out", () => {
+    //it.todo("...tests for zooming out...");
+  });
+});
+
+describe("calculateNodeWeight", () => {
+  const nodeList = [{ id: "A" }, { id: "B" }, { id: "C" }];
+  // @ts-ignore: oh come on
+  let node = Object.assign(...nodeList.map((node) => ({ [node.id]: node })));
+  it.each([
+    [
+      "count links to node",
+      [
+        { source: node.A, target: node.B },
+        { source: node.B, target: node.A },
+        { source: node.C, target: node.A },
+      ],
+      [
+        { node: node.C, expected: 0 },
+        { node: node.B, expected: 1 },
+        { node: node.A, expected: 2 },
+      ],
+    ],
+    [
+      "weight each link by link.value",
+      [
+        { source: node.A, target: node.B, value: 2 },
+        { source: node.B, target: node.A, value: 3 },
+        { source: node.C, target: node.A, value: 0.5 },
+      ],
+      [
+        { node: node.C, expected: 0 },
+        { node: node.B, expected: 2 },
+        { node: node.A, expected: 3.5 },
+      ],
+    ],
+  ])(
+    "should %s",
+    (
+      _test_name: string,
+      links: LinkBetweenHasIDs[],
+      assertions: { node: HasID; expected: number }[]
+    ) => {
+      assertions.forEach(({ node, expected }) => {
+        expect(calculateNodeWeight(node, links)).toEqual(expected);
+      });
+    }
+  );
+});
