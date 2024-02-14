87ac7bc6e81acf4c8e7f91cf48b4d5d8a8c723d6
skep <skep@freq>
2024-02-14 23:46:52 +0100

    feat: align visuals (a little bit) & read-only mode for link-clicking!

--- src/GraphManager/GraphEdit/MarkdownField.tsx
+++ src/GraphManager/GraphEdit/MarkdownField.tsx
@@ -1,5 +1,8 @@
 import { EditorState, LexicalEditor } from "lexical";
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import {
+  InitialConfigType,
+  LexicalComposer,
+} from "@lexical/react/LexicalComposer";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
@@ -20,12 +23,20 @@ import {
   $convertToMarkdownString,
   TRANSFORMERS as TRANSFORMERS_MARKDOWN,
 } from "@lexical/markdown";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
+import NotchedOutline from "@mui/material/OutlinedInput/NotchedOutline";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import { useFormik } from "formik";
 import Box from "@mui/material/Box";
-import { useRef } from "react";
+import {
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
 import { NewNodeForm } from "./PopUp";
 
@@ -48,19 +59,53 @@ export interface MarkdownConfig {
   fieldLabel: string;
   formik: ReturnType<typeof useFormik<NewNodeForm>>;
 }
-const MarkdownEditor = (props: MarkdownConfig) => {
+interface MarkdownEditorConfig extends MarkdownConfig {
+  isEmpty: boolean;
+  setIsEmpty: Dispatch<SetStateAction<boolean>>;
+  isFocused: boolean;
+  // XXX(skep): This is a hack to focus on editor, even if user misses the
+  // editor window. The proper solution should align the editor size exactly
+  // with the NotchedOutline component.
+  onClickStateChange: number;
+}
+
+const FocusWhenStateChangePlugin = ({
+  onClickStateChange,
+}: {
+  onClickStateChange: number;
+}) => {
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    if (onClickStateChange === 0) {
+      editor.setEditable(false);
+      return;
+    }
+    editor.setEditable(true);
+    // XXX(skep): if this timeout hack is not used the user would have to click
+    // 2 times, which is confusing, but why is it needed?!
+    setTimeout(() => editor.focus(), 10);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onClickStateChange]);
+  return null;
+};
+
+const MarkdownEditor = (props: MarkdownEditorConfig) => {
   const theme = {}; // see https://lexical.dev/docs/getting-started/theming
-  const initialConfig = {
+  const loadInitialEditorState = () => {
+    props.setIsEmpty(!props.formik.initialValues.nodeResources);
+    $convertFromMarkdownString(
+      props.formik.initialValues.nodeResources,
+      TRANSFORMERS_MARKDOWN,
+    );
+  };
+  const initialConfig: InitialConfigType = {
     namespace: "NodeResourcesEditor",
     theme,
+    editable: false,
     onError: (...err: any[]) => {
       console.log(...err);
     },
-    editorState: () =>
-      $convertFromMarkdownString(
-        props.formik.initialValues.nodeResources,
-        TRANSFORMERS_MARKDOWN,
-      ),
+    editorState: loadInitialEditorState,
     nodes: [
       AutoLinkNode,
       LinkNode,
@@ -77,22 +122,20 @@ const MarkdownEditor = (props: MarkdownConfig) => {
     _editor: LexicalEditor,
     _tags: Set<string>,
   ) => {
-    //console.log(editorState.toJSON(), editor, tags);
     const helpers = props.formik.getFieldHelpers(props.fieldName);
+    let markdown = "";
     editorState.read(() => {
-      helpers.setValue($convertToMarkdownString(TRANSFORMERS_MARKDOWN));
+      markdown = $convertToMarkdownString(TRANSFORMERS_MARKDOWN);
     });
+    helpers.setValue(markdown);
+    props.setIsEmpty(markdown === "");
   };
   return (
     <Box>
       <LexicalComposer initialConfig={initialConfig}>
         <RichTextPlugin
           contentEditable={<ContentEditable />}
-          placeholder={
-            <Box id={`placeholder-${props.fieldName}`}>
-              Add the resources you used for learning!
-            </Box> /*TODO: translate*/
-          }
+          placeholder={<></>}
           ErrorBoundary={LexicalErrorBoundary}
         />
         <MarkdownShortcutPlugin transformers={TRANSFORMERS_MARKDOWN} />
@@ -100,26 +143,51 @@ const MarkdownEditor = (props: MarkdownConfig) => {
         <LinkPlugin validateUrl={validateUrl} />
         <AutoLinkPlugin matchers={MATCHERS} />
         <HistoryPlugin />
+        <FocusWhenStateChangePlugin
+          onClickStateChange={props.onClickStateChange}
+        />
       </LexicalComposer>
+      <NotchedOutline
+        notched={true}
+        focused={props.isFocused}
+        label={props.isFocused || !props.isEmpty ? props.fieldLabel : ""}
+      />
     </Box>
   );
 };
 export const MarkdownEditorWrapper = (props: MarkdownConfig) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const handleClick = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
+  const [isFocused, setIsFocused] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [onClickStateChange, setOnClickStateChange] = useState(0);
+  const handleClick: MouseEventHandler<HTMLDivElement> = (_) => {
+    setOnClickStateChange(1);
+    setIsFocused(true);
+  };
+  const handleUnfocus = () => {
+    setIsFocused(false);
+    setOnClickStateChange(0);
   };
   return (
-    <FormControl sx={{ padding: 1, paddingTop: 2 }} onClick={handleClick}>
+    <FormControl
+      fullWidth={true}
+      sx={{ padding: 1 }}
+      onClick={handleClick}
+      onBlur={handleUnfocus}
+    >
       <InputLabel
         htmlFor={props.fieldName} /*TODO(skep): needs focused=, etc.*/
+        focused={isFocused}
+        shrink={isFocused || !isEmpty}
       >
         {props.fieldLabel}
       </InputLabel>
-      {/*<FormHelperText id={props.fieldName}>{props.fieldLabel}</FormHelperText>*/}
-      <MarkdownEditor {...props} />
+      <MarkdownEditor
+        {...props}
+        isEmpty={isEmpty}
+        setIsEmpty={setIsEmpty}
+        isFocused={isFocused}
+        onClickStateChange={onClickStateChange}
+      />
     </FormControl>
   );
 };
