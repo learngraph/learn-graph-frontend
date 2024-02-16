598a95abcfec993c048b34d37ca4828e6f0979ae
suxatcode <nnamegah21@gmx.de>
2024-02-16 19:53:43 +0100

    add 1 screenshot & route to /howto

---
+++ src/HowToLearngraph.tsx
@@ -0,0 +1,5 @@
+import Image from 'mui-image'
+
+export const HowToLearngraph = () => {
+    return <Image src="HowTo-link.png" />
+}
--- src/LearngraphOrgRoutes.tsx
+++ src/LearngraphOrgRoutes.tsx
@@ -1,12 +1,14 @@
 import { GraphManager } from "./GraphManager/GraphManager";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { About } from "./About";
+import { HowToLearngraph } from "./HowToLearngraph";
 
 export const LearngraphOrgRoutes = () => {
   return (
     <Routes>
       <Route path="/about" Component={About} />
       <Route path="/" Component={GraphManager} />
+      <Route path="/howto" Component={HowToLearngraph} />
       <Route
         path="*"
         element={
