974d3f8de24e5dce441f02b6ef2290f06cc1b824
Enno Wellmann <eh.wellmann@gmx.de>
2024-02-25 22:11:06 +0100

    HowTo: add an App bar to the top of the screen.

--- src/HowToLearngraph.tsx
+++ src/HowToLearngraph.tsx
@@ -1,5 +1,28 @@
+import { AppBar, Toolbar } from '@mui/material'
 import Image from 'mui-image'
+import { LearngraphLOGO } from './GraphManager/components/HeaderBar'
+import { useNavigate } from 'react-router-dom'
 
 export const HowToLearngraph = () => {
-    return <Image src="HowTo-link.png" />
+    const navigate = useNavigate();
+    return (
+        <>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <LearngraphLOGO
+                            onClick={() => navigate('/')}
+                            sx={{
+                                ':hover': {
+                                cursor: "pointer",
+                                },
+                            }} 
+                        />
+                    </Toolbar>
+            
+                </AppBar>
+            Here should the Tutorial Text go
+            <Image src="HowTo-link.png"
+            width={800} />
+        </>
+    )
 }
