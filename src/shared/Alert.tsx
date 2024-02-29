84d57e5eca5c899d75434f7cd207893f14535ae9
suxatcode <nnamegah21@gmx.de>
2024-02-29 13:27:27 +0100

    add Alert.tsx

---
+++ src/shared/Alert.tsx
@@ -0,0 +1,39 @@
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
+
+export const AlertPopupBar = () => {
+  const [open, setOpen] = useState(false);
+  const handleClose = (_: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+  const action = (
+    <>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      message="Note archived"
+      action={action}
+    />
+  );
+};
