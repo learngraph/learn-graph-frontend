83bbe5bcbaaf539fcf7eb8c6ad29f1d802565e09
Enno Wellmann <eh.wellmann@gmx.de>
2024-03-22 11:00:29 +0100

    chore handover :)

--- src/HowToLearngraph.tsx
+++ src/HowToLearngraph.tsx
@@ -1,28 +1,29 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Box, Toolbar, useTheme } from '@mui/material'
 import Image from 'mui-image'
+import { NavigationWithContent } from './Navigation'
+
 //import { LearngraphLOGO } from './GraphManager/components/HeaderBar'
 //import { useNavigate } from 'react-router-dom'
 
 export const HowToLearngraph = () => {
-    //const navigate = useNavigate();
+    const theme=useTheme()
     return (
         <>
-                <AppBar position='static'>
-                    <Toolbar>
-                        {/* <LearngraphLOGO
-                            onClick={() => navigate('/')}
-                            sx={{
-                                ':hover': {
-                                cursor: "pointer",
-                                },
-                            }} 
-                        /> */}
-                    </Toolbar>
-            
-                </AppBar>
-            Here should the Tutorial Text go
-            <Image src="HowTo-link.png"
-            width={800} />
+            <NavigationWithContent
+                withSideNavigation={true}
+                content={
+                    <Box
+                        sx={{padding:theme.spacing(2)}} >
+
+                        The Learngraph is a map of Learning dependencies. In this image you can see that in order
+                        to understand multiplication you need to learn about addition.
+                        <Image src="HowTo-link.png"
+                            width={800} />
+                        There is no way around learning addition first so the connection between them is strong.
+                    </Box>
+                }
+            />
+
         </>
     )
 }
