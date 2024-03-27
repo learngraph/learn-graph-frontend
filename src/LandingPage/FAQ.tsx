d752da4cf517d6096e9c03eb1c7a7bb8a2271c36
JLamperti <jonas.lamperti@web.de>
2024-03-27 02:08:02 +0100

    fix: prefix unused var with _

--- src/LandingPage/FAQ.tsx
+++ src/LandingPage/FAQ.tsx
@@ -41,7 +41,7 @@ export default function FAQ() {
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
     };
 
