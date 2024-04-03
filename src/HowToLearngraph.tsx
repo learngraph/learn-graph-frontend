3cd8b45402c68c7a9ec18631d1d9a63332208467
suxatcode <nnamegah21@gmx.de>
2024-04-03 22:33:29 +0200

    doc: /howto + translation description

--- src/HowToLearngraph.tsx
+++ src/HowToLearngraph.tsx
@@ -21,6 +21,20 @@ const HowToImage = (props: { availableSpace: Rectangle; src: string }) => {
 const HowToTypography = (props: { children: ReactNode }) => {
   return <Typography sx={{ maxWidth: "100ch" }}>{props.children}</Typography>;
 };
+const HowToTopic = (props: { children: ReactNode }) => {
+  const theme = useTheme();
+  return (
+    <Typography
+      sx={{
+        paddingBottom: theme.spacing(2),
+        paddingTop: theme.spacing(2),
+      }}
+      variant="h2"
+    >
+      {props.children}
+    </Typography>
+  );
+};
 
 export const HowToLearngraph = () => {
   const theme = useTheme();
@@ -49,11 +63,6 @@ export const HowToLearngraph = () => {
               width: "100%",
             }}
             component="main"
-            /* sx={(theme) => ({
-              flexGrow: 1,
-              padding: 3,
-              backgroundColor: theme.palette.background.default,
-            })}*/
             ref={wrapperRef}
           >
             <Typography sx={{ paddingBottom: theme.spacing(4) }} variant="h1">
@@ -72,19 +81,24 @@ export const HowToLearngraph = () => {
               the arrow. Clicking on a topic reveals learning resources and
               infos about the most important learning dependencies.
             </HowToTypography>
-            <Typography
-              sx={{
-                paddingBottom: theme.spacing(2),
-                paddingTop: theme.spacing(2),
-              }}
-              variant="h2"
-            >
-              Add your knowledge
-            </Typography>
+            <HowToTopic> Translations </HowToTopic>
+            <HowToImage
+              src="HowTo-translation-de-en.png"
+              availableSpace={availableSpace}
+            />
+            <HowToTypography>
+              You may find some subjects in a different language than your
+              selected one. They are marked by the corresponding country flag.
+              You can translate subjects to your selected language by editing
+              the subject and removing the flag icon. To insert a translation in
+              a different language than your currently selected on, first change
+              your language settings and then proceed with the translation.
+            </HowToTypography>
+            <HowToTopic>Add your knowledge</HowToTopic>
             <HowToTypography>
               The <Edit fontSize="small" /> icon lets you switch between edit
-              and read-only mode. Move the abstract concept toward its
-              prerequisite knowledge to create a new link. Alternatively use the{" "}
+              and read-only mode. Move the subject toward its prerequisite
+              knowledge to create a new link. Alternatively use the{" "}
               <Add fontSize="small" /> icon &gt; "New depency" dialouge Select
               an existing link to vote on the strength of the connection. To add
               a new Topic use the <Add fontSize="small" /> icon &gt; "New
