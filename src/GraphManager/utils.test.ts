4a8792a1261093286b30a771f8356ad52365dc4a
suchechaya123 <goadip@aufu.de>
2024-05-26 21:46:45 +0200

    onGraphUpdate Testing
    First test always passes (broken),
    second test is broken

--- src/GraphManager/utils.test.ts
+++ src/GraphManager/utils.test.ts
@@ -5,6 +5,7 @@ import {
   nodeCanvasObject,
   makeGraphState,
   convertBackendGraphToForceGraph,
+  onGraphUpdate,
   setGraphSize,
   nodePointerAreaPaint,
   linkPointerAreaPaint,
@@ -327,5 +328,18 @@ describe("makeOnNodeHover", () => {
       source: [],
       target: [],
     });
+  });
+});
+
+
+describe("onGraphUpdate", () => {
+  it("should update the graph data", () => {
+    const ctrl = makeMockController();
+    const graphData = { nodes: [], links: [] };
+    const setGraph = jest.fn();
+    // @ts-ignore
+    onGraphUpdate( ctrl, { graph: graphData }, setGraph);
+    expect(ctrl.graph.current).toEqual(graphData);
+    expect(setGraph).toHaveBeenCalledWith(makeGraphState(graphData, jest.fn()));
   });
 });
