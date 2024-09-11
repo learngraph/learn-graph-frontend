76b7c9a776b26fceccb3305dd5c44d993e8a8496
skep <skep@lgc>
2024-09-11 18:03:35 +0000

    we understood the goal, but no implementation yet!

---
+++ src/GraphManager/GraphEdit/MarkdownField.test.tsx
@@ -0,0 +1,5 @@
+describe("validateUrl", () => {
+  it("should match an ugly url with ()-braces!", () => {
+    expect(1).toBe(1);
+  })
+})
--- src/GraphManager/GraphEdit/MarkdownField.tsx
+++ src/GraphManager/GraphEdit/MarkdownField.tsx
@@ -40,7 +40,7 @@ import { useTheme } from "@emotion/react";
 const urlRegExp = new RegExp(
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/,
 );
-const validateUrl = (url: string): boolean => {
+export const validateUrl = (url: string): boolean => {
   return url === "https://" || urlRegExp.test(url);
 };
 
@@ -119,6 +119,13 @@ const MarkdownEditor = (props: MarkdownEditorConfig) => {
     editorState.read(() => {
       markdown = $convertToMarkdownString(TRANSFORMERS_MARKDOWN);
     });
+    // TODO:
+    //if containsUrl(markdown) {
+    //  markdown = fixLinkIssue(markdown);
+    //}
+    // 1) http://localhost:3000/graph <- has ()-braces? no
+    // 2) [http://localhost:3000/grap(h)](http://localhost:3000/grap%27h%28)
+    // 3) http://localhost:3000/grap(h) <- has braces must be changed
     props.setValueOnChange(markdown);
     props.setIsEmpty(markdown === "");
   };
