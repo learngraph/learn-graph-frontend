196e4efb02d37943169110dac5165dbe62d12f61
Enno Wellmann <eh.wellmann@gmx.de>
2024-10-18 17:00:15 +0200

    Weiter aufgeräumt

--- src/GraphManager/GraphRendererSigma.tsx
+++ src/GraphManager/GraphRendererSigma.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 
 import { Controller } from "./GraphEdit/GraphEdit";
-import { SigmaContainer, useLoadGraph, useSigma } from "@react-sigma/core";
+import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/react-sigma.min.css";
-import { MultiDirectedGraph, MultiDirectedGraph as SigmaGraph } from "graphology";
+import { MultiDirectedGraph } from "graphology";
 import { useGraphologyGraphData } from "./RPCHooks/useGraphData";
 import {
   DEFAULT_EDGE_CURVATURE,
@@ -16,16 +16,15 @@ interface GraphRendererProps {
   backgroundColor: "black" | "white";
   graphData: any;
   sigmaStyle: { height: number; width: number };
-q}
+}
 
 export const GraphologyGraph = () => {
-  const sigma = useSigma(); // Use Sigma's instance
+  // const sigma = useSigma(); // Use Sigma's instance
   
   const { data, queryResponse } = useGraphologyGraphData(); // Fetch graph data using custom hook
   const loadGraph = useLoadGraph();
-  const [graph, setGraphData] = useState<MultiDirectedGraph | null>(null);
   useEffect(() => {
-    if (data) {
+    if (data) {// I should have a load graph routine here that loads the graph everytime the data or the graphology instance changes
       const initialGraph = new MultiDirectedGraph(); // This needs to be available for createNodeAtPosition()
       initialGraph.import(data);
       console.log("hi?");
@@ -37,6 +36,7 @@ export const GraphologyGraph = () => {
           curvature: DEFAULT_EDGE_CURVATURE, // Ensure DEFAULT_EDGE_CURVATURE is defined
         });
       });
+      loadGraph(initialGraph)
     }
   }, [/*loadGraph, */ queryResponse]); // This now reloads the Graph whenever the query response changes...
 
@@ -63,52 +63,3 @@ export const GraphRendererSigma: React.FC<GraphRendererProps> = () => {
 };
 
 export default GraphRendererSigma;
-
-// export const DisplaySigma: React.FC<GraphRendererProps> = ({
-//   controller,
-//   backgroundColor,
-// }) => {
-//   controller.language; // for the linter...
-//   const containerRef = useRef<HTMLDivElement | null>(null); // Ref for Sigma container
-//   const { data, queryResponse } = useGraphologyGraphData(); // Fetch graph data using custom hook
-
-//   //// -------- Graph rendering ----------
-//   useEffect(() => {
-//     // Ensure the data is available and the container is ready
-//     if (data && containerRef.current) {
-//       const graph = new Graph();
-
-//       // `data` must have Graphology's format
-//       graph.import(data);
-//       const renderer = new Sigma(graph, containerRef.current, {
-//         allowInvalidContainer: true, // Optional: handles cases where the container might not be ready immediately
-//         defaultEdgeType: "curve",
-//         edgeProgramClasses: {
-//           curve: EdgeCurvedArrowProgram,
-//         },
-//       });
-
-//       // Cleanup on component unmount
-//       return () => {
-//         renderer.kill(); // Kill the renderer to avoid memory leaks
-//       };
-//     }
-//   }, [data, availableSpace]); // Recalculate graph size when data or available space changes
-
-//   // Handle loading and error states
-//   if (queryResponse.loading) return <div>Loading graph...</div>;
-//   if (queryResponse.error)
-//     return <div>Error loading graph: {queryResponse.error.message}</div>;
-
-//   return (
-//     <Box
-//       ref={containerRef}
-//       sx={{
-//         height: "100vh", // Full viewport height
-//         width: "100vw", // Full viewport width
-//         position: "relative",
-//         background: backgroundColor,
-//       }}
-//     ></Box>
-//   );
-// };
