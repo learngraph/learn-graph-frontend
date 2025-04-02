5986c07f0ad77a149af82735f825924da813e2aa
Laurin <42897588+suxatcode@users.noreply.github.com>
2025-04-02 20:01:13 +0200

    fix: production env setup (#132)

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import { PostHogProvider } from "posthog-js/react";
 import { POSTHOG_API_KEY, POSTHOG_HOST } from "./constants";
 
 export const App = () => {
+  if (!POSTHOG_API_KEY) {
+    console.error("missing POSTHOG_API_KEY environemnt variable");
+  }
+  if (!POSTHOG_HOST) {
+    console.error("missing POSTHOG_HOST environemnt variable");
+  }
   return (
     <PostHogProvider
-      apiKey={POSTHOG_API_KEY}
+      apiKey={POSTHOG_API_KEY || ""}
       options={{
         api_host: POSTHOG_HOST,
         autocapture: true,
