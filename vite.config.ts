8c02fd29025ad4a694d7f0e180e6697098595da5
skep <skep@lgc>
2025-04-11 20:18:22 +0000

    chore: generate sitemap for SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
+
+/* Update sitemap.xml:
+```sh
+grep -o '"/[^"]*"' src/LearngraphOrgRoutes.tsx| sed -e 's,.*"/\([^"]*\)".*,\t"/\1"\,,g'|grep -v :
+```
+*/
+const sites = [
+  "/university",
+  "/nations",
+  "/industry",
+  "/ecosystem",
+  "/k-12",
+  "/contact",
+  "/contact-us",
+  "/impact",
+  "/wirkung",
+  "/über-uns",
+  "/about",
+  "/imprint",
+  "/impressum",
+  "/terms-of-use",
+  "/privacy-policy",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), Sitemap({ dynamicRoutes: sites })],
   resolve: {
     alias: {
       "@src": "/src",
